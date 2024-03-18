c717501d632a00314b08c0e3993addf2439abeb0
Luiz Fernando Forte <LFFORTE@GMAIL.COM>
2024-03-18 11:00:32 -0300

    Exercício de aula renderização de listas

--- src/components/ListaTarefas/index.js
+++ src/components/ListaTarefas/index.js
@@ -12,15 +12,21 @@ import bin from "../../assets/bin.png";
 
 export function ListaTarefas() {
   const [novaTarefa, setNovaTarefa] = useState("");
+  const [lista, setLista] = useState([]);
 
 
   const onChangeTarefa = (event) => {
     setNovaTarefa(event.target.value);
   };
 
-  const adicionaTarefa = () => {};
-
-  const removeTarefa = () => {};
+  const adicionaTarefa = () => {
+    setLista([...lista, novaTarefa]);
+    setNovaTarefa('');
+  };
+  const removeTarefa = (item) => {
+    const listaFiltrada = lista.filter((cadaTarefa) => cadaTarefa !== item);
+    setLista(listaFiltrada);
+  };
 
   return (
     <ListaTarefasContainer>
@@ -30,17 +36,19 @@ export function ListaTarefas() {
           value={novaTarefa}
           onChange={onChangeTarefa}
         />
-        <AddTaskButton>Adicionar</AddTaskButton>
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
       </InputContainer>
 
       <ListaContainer>
         <ul>
-          <Tarefa>
-            <p>Nova tarefa</p>
-            <RemoveButton>
-              <img src={bin} alt="" width="16px" />
-            </RemoveButton>
-          </Tarefa>
+          {lista.map((item, id) => {
+            return <Tarefa>
+              <p>{item.toUpperCase()}</p>
+              <RemoveButton onClick={() => removeTarefa(item)}>
+                <img src={bin} alt="" width="16px" />
+              </RemoveButton>
+            </Tarefa>
+          })}
         </ul>
       </ListaContainer>
     </ListaTarefasContainer>
